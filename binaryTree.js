2870dbd4fdce294986dd718b2bffcc5d12c73588
DaltonHart <daltonvaren@gmail.com>
2022-06-19 23:31:49 -0700

    :sparkles: (binaryTree) adds base of b…

---
+++ binaryTree.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert() {}
+  lookup() {}
+  remove() {}
+}
